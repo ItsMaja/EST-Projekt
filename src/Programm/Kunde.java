72172057cd60c3f3468097ce80e7829c3f6d818c
Maja <majawandura@web.de>
2019-04-28 20:03:41 +0200

    Redundanten Code ersetzt, Kunde-Klasse erhält statische Variable zum Vorhalt der neuen Kundennummern

--- src/Programm/Kunde.java
+++ src/Programm/Kunde.java
@@ -1,19 +1,14 @@
 package Programm;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Random;
-import java.util.TreeMap;
-
 /**
  * @author Jonathan Baumann
  * @version 1
  */
 public class Kunde {
 
-	public static ArrayList<String> kunden = new ArrayList();
-	public static Map<String, Integer> liste = new TreeMap();
-	
+	// Hält die Nummer für den nächsten Kunden vor
+	static private int naechsteKundennummer = 1000;
+
 	private String name;
 	private String ort;
 	private String strasse;
@@ -23,20 +18,15 @@ public class Kunde {
 	private int kundenNummer;
 	
 	
-	public Kunde(String name, String ort, String strasse, int postFach, int hausNummer, int plz) {
+	Kunde(String name, String ort, String strasse, int postFach, int hausNummer, int plz) {
+		this.kundenNummer = naechsteKundennummer;
+		naechsteKundennummer++;
 		this.name = name;
 		this.ort = ort;
 		this.strasse = strasse;
 		this.postFach = postFach;
 		this.hausNummer = hausNummer;
-		if(kunden.contains(name)) {
-			this.kundenNummer = liste.get(name);
-		} else {
-			kunden.add(name);
-			Random zufall = new Random();
-			this.kundenNummer = zufall.nextInt(90000);
-			liste.put(name, kundenNummer);
-		}
+		this.plz = plz;
 	}
 	
 	public void setName(String name) {
